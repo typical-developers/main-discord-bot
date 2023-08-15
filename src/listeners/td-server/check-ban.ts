7d6b94ba5ee491623135d9c004f5fee546f2f33d
Colt <39076891+LuckFire@users.noreply.github.com>
2023-08-15 16:49:35 -0400

    feat: check for bans in td server
    this is for the feedback server, will automatically give them a read-only role.

---
+++ src/listeners/td-server/check-ban.ts
@@ -0,0 +1,18 @@
+import { Listener } from '@sapphire/framework';
+import { ApplyOptions } from '@sapphire/decorators';
+import { Events, GuildMember } from 'discord.js';
+
+@ApplyOptions<Listener.Options>({
+	event: Events.GuildMemberAdd,
+	once: false
+})
+export class ReadyListener extends Listener {
+	public override async run(member: GuildMember) {
+        if (member.guild.id !== '893717531179769887') return;
+
+        const BANNED = (await this.container.client.guilds.fetch('865737627712749579')).bans.fetch(member.id).catch(() => null);
+        if (!BANNED) return;
+
+        member.roles.add('1132539246276784208');
+	}
+}
