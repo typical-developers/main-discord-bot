62a081e275ff5ab881a418781a068d47e0803941
LuckFire <39076891+LuckFire@users.noreply.github.com>
2024-04-22 16:23:58 -0400

    refactor: abbreviate
    return if the value is <= 1000, reducing the line indents

--- src/lib/util/abbreviate.ts
+++ src/lib/util/abbreviate.ts
@@ -5,24 +5,23 @@
  */
 export function abbreviateNumber(value: number): string {
     let newValue: string = value.toString();
+    if (value <= 1000) return newValue;
 
-    if (value >= 1000) {
-        const suffixes: string[] = ['', 'k', 'm', 'b', 't'];
-        const suffixNum: number = Math.floor(newValue.length / 3);
-        let shortValue: number | string = '';
+    const suffixes: string[] = ['', 'k', 'm', 'b', 't'];
+    const suffixNum: number = Math.floor(newValue.length / 3);
+    let shortValue: number | string = '';
 
-        for (let precision: number = 2; precision >= 1; precision--) {
-            shortValue = parseFloat((suffixNum !== 0 ? value / Math.pow(1000, suffixNum) : value).toPrecision(precision));
-            const dotLessShortValue: string = (shortValue + '').replace(/[^a-zA-Z 0-9]+/g, '');
+    for (let precision: number = 2; precision >= 1; precision--) {
+        shortValue = parseFloat((suffixNum !== 0 ? value / Math.pow(1000, suffixNum) : value).toPrecision(precision));
+        const dotLessShortValue: string = (shortValue + '').replace(/[^a-zA-Z 0-9]+/g, '');
 
-            if (dotLessShortValue.length <= 2) {
-                break;
-            }
+        if (dotLessShortValue.length <= 2) {
+            break;
         }
-
-        if (typeof shortValue !== 'string' && shortValue % 1 !== 0) shortValue = shortValue.toFixed(1);
-        newValue = shortValue + suffixes[suffixNum];
     }
+
+    if (typeof shortValue !== 'string' && shortValue % 1 !== 0) shortValue = shortValue.toFixed(1);
+    newValue = shortValue + suffixes[suffixNum];
 
     return newValue;
 }
