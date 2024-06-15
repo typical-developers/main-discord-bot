c58bab3f848f9c9e9ac2706bb64c58b948aa09ef
LuckFire <39076891+LuckFire@users.noreply.github.com>
2024-06-15 19:14:14 -0400

    docs: voice room utils

--- src/lib/util/voice-rooms.ts
+++ src/lib/util/voice-rooms.ts
@@ -2,7 +2,14 @@ import { container } from "@sapphire/pieces";
 import { VoiceRoomDetails } from "@typical-developers/api-types/graphql";
 import { ActionRowBuilder, ButtonBuilder, ButtonStyle, Colors, ComponentType, EmbedBuilder, inlineCode } from "discord.js";
 
-export async function isOwner(guildId: string, channelId: string, userId: string) {
+/**
+ * Check if a user owns the voice room.
+ * @param guildId The id for the guild.
+ * @param channelId The id for the channel attempting to be modified.
+ * @param userId The id of the user attempting to modify the voice channel state.
+ * @returns {Promise<boolean>} Whether the channel can be modified or not.
+ */
+export async function isOwner(guildId: string, channelId: string, userId: string): Promise<boolean> {
     const roomDetails = await container.api.getVoiceRoom(guildId, channelId);
     if (!roomDetails) return false;
 
@@ -11,7 +18,12 @@ export async function isOwner(guildId: string, channelId: string, userId: string
         : false;
 }
 
-export function voiceRoomInfoEmbed(settings: VoiceRoomDetails) {
+/**
+ * Generate an information embed for the voice room settings.
+ * @param settings The settings for the voice room.
+ * @returns {{ embeds: EmbedBuilder[]; components: ActionRowBuilder<ButtonBuilder>[]; }}}
+ */
+export function voiceRoomInfoEmbed(settings: VoiceRoomDetails): { embeds: EmbedBuilder[]; components: ActionRowBuilder<ButtonBuilder>[]; } {
     const embed = new EmbedBuilder({
         color: Colors.Red,
         title: '🔊 Voice Room Management',
