85965093e39081dd0075b0720280647a7c1020a6
LuckFire <39076891+LuckFire@users.noreply.github.com>
2024-07-07 20:09:03 -0400

    feat: cycling bot stauts

---
+++ src/lib/types/enums.ts
@@ -0,0 +1,10 @@
+export const enum BotStatusCycle {
+    OaklandsPlaying = 'OAKLANDS_PLAYING',
+    DisasterDefensePlayng = 'DISASTER_DEFENSE_PLAYING'
+}
+
+export const enum ExperienceUniverseID {
+    Oaklands = 3666294218,
+    DisasterDefense = 5972791957,
+    StudDefense = 2
+};
--- src/listeners/BotStatus.ts
+++ src/listeners/BotStatus.ts
@@ -1,45 +1,69 @@
 import { Listener } from '@sapphire/framework';
 import { ApplyOptions } from '@sapphire/decorators';
 import { type Client, type ActivityOptions, Events, ActivityType } from 'discord.js';
+import { BotStatusCycle, ExperienceUniverseID } from '#lib/types/enums';
 
 @ApplyOptions<Listener.Options>({
 	event: Events.ClientReady,
 	once: true
 })
 export class BotStatus extends Listener {
-    private playingCache: { [key: string]: number } = {};
     private activity: ActivityOptions = { type: ActivityType.Watching, name: '' };
+    private playingCache: { [key: string]: number } = {};
+    private currentStatus: BotStatusCycle = BotStatusCycle.OaklandsPlaying;
 
-    private async fetchTotalPlaying(universeId: number) {
-        const response = await fetch(`https://games.roblox.com/v1/games?universeIds=${universeId}`).catch(() => null);
+    private async recachePlayers(universeIds: number[]) {
+        const response = await fetch(`https://games.roblox.com/v1/games?universeIds=${universeIds.join(',')}`).catch(() => null);
 
         if (!response || !response.ok) return null;
 
-        const { data } = await response.json();
-        const experience  = data[0];
+        const { data } = await response.json() as {
+            // There's more data to this but this is everything the function needs.
+            data: {
+                id: number;
+                name: string;
+                description: string;
+                playing: number;
+            }[]
+        };
 
-        if (!experience) return null;
-        if (this.playingCache[universeId] === experience.playing) return null;
+        if (!data.length) return null;
 
-        this.playingCache[universeId] = experience.playing;
-        return experience.playing;
+        this.playingCache = data.reduce((acc, curr) => ({...acc, [curr.id]: curr.playing}), {});
     }
 
-    public async statusRun(client: Client) {
-        const oaklandsPlaying = await this.fetchTotalPlaying(3666294218);
 
-        if (!oaklandsPlaying) {
-            return new Promise((res, _rej) => 
-                setTimeout(async () => res(await this.statusRun(client)), 5 * 1000)
-            );
+    public async statusRun(client: Client) {
+        await this.recachePlayers([ExperienceUniverseID.Oaklands, ExperienceUniverseID.DisasterDefense]);
+
+        switch (this.currentStatus) {
+            case BotStatusCycle.OaklandsPlaying:
+                const oaklandsPlayers = this.playingCache[ExperienceUniverseID.Oaklands];
+                if (!oaklandsPlayers) break;
+
+                this.currentStatus = BotStatusCycle.DisasterDefensePlayng;
+                this.activity = { name: `Oaklands・${oaklandsPlayers} playing` };
+
+                break;
+            case BotStatusCycle.DisasterDefensePlayng:
+                const ddPlayers = this.playingCache[ExperienceUniverseID.DisasterDefense];
+                if (!ddPlayers) break;
+
+                this.currentStatus = BotStatusCycle.OaklandsPlaying;
+                this.activity = { name: `Disaster Defense・${ddPlayers} playing` };
+
+                break;
         }
 
-        this.activity.name = `Oaklands・${oaklandsPlaying} playing`;
-        client.user?.setActivity(this.activity);
+        if (client.user?.presence.activities[0]?.name !== this.activity.name) {
+            client.user?.setActivity(this.activity);
+        }
 
-        return new Promise((res, _rej) => 
-            setTimeout(async () => res(await this.statusRun(client)), 5 * 1000)
-        );
+        return new Promise((res) => {
+            setTimeout(async () => {
+                res(await this.statusRun(client));
+            }, 5 * 1000);
+        });
     }
 
     public override async run(client: Client) {
