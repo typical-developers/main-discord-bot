bdf3d509a27b0461eb25b597fc19c479f6b94db6
LuckFire <39076891+LuckFire@users.noreply.github.com>
2024-08-01 18:06:13 -0400

    feat: add select menu on same message

--- src/interaction-handlers/menus/MessageEmbedSelector.ts
+++ src/interaction-handlers/menus/MessageEmbedSelector.ts
@@ -1,6 +1,15 @@
 import { InteractionHandler, InteractionHandlerTypes } from '@sapphire/framework';
 import { ApplyOptions } from '@sapphire/decorators';
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle, StringSelectMenuInteraction } from 'discord.js';
+import {
+    ActionRow,
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    ComponentType,
+    StringSelectMenuBuilder,
+    StringSelectMenuComponent,
+    StringSelectMenuInteraction
+} from 'discord.js';
 import { createMessageEmbed, getMessageContent, parseMessageLink } from '#lib/util/message-embeds';
 
 @ApplyOptions<InteractionHandler.Options>({
@@ -11,6 +20,42 @@ export class MessageEmbedSelector extends InteractionHandler {
         if (interaction.customId !== 'MessageEmbedSelector') return this.none();
 
         return this.some();
+    }
+
+    private getMessageMenu(components: StringSelectMenuInteraction['message']['components'], defaultLink: string) {
+        const menu = components.find(row =>
+            row.components.some(({ type, customId }) =>
+                type === ComponentType.StringSelect
+                && customId === 'MessageEmbedSelector'
+            )
+        ) as ActionRow<StringSelectMenuComponent> | undefined;
+
+        if (!menu) {
+            return null;
+        }
+
+        const newOptions = menu.components[0].options.reduce((acc, curr) => {
+            if (curr.value === defaultLink) {
+                curr.default = true;
+            }
+            else {
+                curr.default = false;
+            }
+            
+            return acc
+        }, menu.components[0].options);
+
+        // Originally, I was going to try reusing the old component.
+        // However, they're in a read-only state, which made me realize they shouldn't be modified.
+        // Instead, we just make a new row and use that.
+        const otherMessages = new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(
+            new StringSelectMenuBuilder({
+                customId: "MessageEmbedSelector",
+                options: newOptions
+            })
+        )
+        
+        return otherMessages;
     }
 
     public async run(interaction: StringSelectMenuInteraction) {
@@ -31,19 +76,23 @@ export class MessageEmbedSelector extends InteractionHandler {
             });
         };
 
-        const embed = await createMessageEmbed(message);
-        const jumpToButton = new ActionRowBuilder<ButtonBuilder>()
-            .addComponents(
-                new ButtonBuilder({
-                    style: ButtonStyle.Link,
-                    label: 'Jump to Original',
-                    url: `https://discord.com/channels/${messageLink.guildId}/${messageLink.channelId}/${messageLink.messageId}`
-                })
-            );
+        const formattedLink = `https://discord.com/channels/${messageLink.guildId}/${messageLink.channelId}/${messageLink.messageId}`;
 
-        return await interaction.editReply({
-            embeds: embed,
-            components: [jumpToButton]
-        });
+        const embeds = await createMessageEmbed(message);
+        const jumpToButton = new ActionRowBuilder<ButtonBuilder>().addComponents(new ButtonBuilder({
+                style: ButtonStyle.Link,
+                label: 'Jump to Original',
+                url: formattedLink
+            })
+        );
+
+        const otherMessages = this.getMessageMenu(interaction.message.components, formattedLink)
+        const components: ActionRowBuilder<ButtonBuilder | StringSelectMenuBuilder>[] = [jumpToButton];
+
+        if (otherMessages) {
+            components.push(otherMessages);
+        }
+
+        return await interaction.editReply({ embeds, components });
     }
 }
