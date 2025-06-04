26beecec6f5f79539098a5a3027dc8c5da18d040
LuckFire <39076891+LuckFire@users.noreply.github.com>
2025-06-04 01:02:31 -0400

    refactor: update env info

--- src/lib/types/env.d.ts
+++ src/lib/types/env.d.ts
@@ -1,25 +1,46 @@
 declare module Bun {
     interface Env {
-        /** The API url. */
-        API_URL: string;
-
-        /** Whether or not the current deployment is a dev build. */
-        DEV_DEPLOYMENT: 'true' | 'false';
-
-        /** The Discord bot's token. */
+        /**
+         * The current deployment environment that the bot is running on.
+         * @default 'production' This is the default to prevent accidents.
+         */
+        ENVIRONMENT: 'production' | 'development';
+        /**
+         * The Discord token for the bot.
+         */
         DISCORD_TOKEN: string;
-        /** The API key for accessing the bot's backend. */
-        BOT_ENDPOINT_API_KEY: string;
-        /** The API secret for accessing the experience's backend. */
-        EXPERIENCE_ENDPOINT_API_SECRET: string;
-        /** The webhook for logging errors. */
+        /**
+         * The Discord webhook url to push error information to.
+         */
         BOT_ERROR_WEBHOOK_URL: string;
 
-        /** Redis Username */
-        REDIS_USERNAME: string;
-        /** Redis Password */
-        REDIS_PASSWORD: string;
-        /** Redis host (host:port) */
+        /**
+         * The base url for the public experience api.
+         */
+        PUBLIC_API_URL: string;
+        
+        /**
+         * The base url for the bot's internal api.
+         */
+        BOT_API_URL: string;
+        /**
+         * The authorization for `BOT_API_URL`.
+         */
+        BOT_ENDPOINT_API_KEY: string;
+
+        /**
+         * The host for the Redis connection.
+         * To do a specific DB, for example, do `127.0.0.1:6379/1`.
+         */
         REDIS_HOST: string;
+        /**
+         * The username to connect to the Redis instance.
+         */
+        REDIS_USERNAME: string;
+        /**
+         * The password, if nay, to connect to the Redis instance.
+         * If there is no password, leave empty or put `nopass`.
+         */
+        REDIS_PASSWORD?: string;
     }
 }
