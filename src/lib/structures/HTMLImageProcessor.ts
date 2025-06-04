b9666ee8d08c622af9f9291ed393e3310e805b65
LuckFire <39076891+LuckFire@users.noreply.github.com>
2025-06-04 19:43:40 -0400

    fix: actually move chrome puppeteer cluster doesnt support websocket connections in the future, will have to custom implement it if it calls for it

--- src/lib/structures/HTMLImageProcessor.ts
+++ src/lib/structures/HTMLImageProcessor.ts
@@ -1,6 +1,4 @@
-import puppeteer, { type LaunchOptions } from "puppeteer";
-import Handlebars from "handlebars";
-import { Cluster } from "puppeteer-cluster";
+import puppeteer, { Browser, type ConnectOptions } from "puppeteer";
 
 interface DrawOptions<T> {
     html: string;
@@ -10,27 +8,10 @@ interface DrawOptions<T> {
 }
 
 export default class HTMLImageProcessor {
-    private _cluster: Cluster;
+    private readonly _browser: Browser;
 
-    constructor(cluster: Cluster) {
-        this._cluster = cluster;
-
-        this._cluster.task(async ({ page, data: { html, transparency, savePath } }) => {
-            await page.setContent(html, { waitUntil: 'networkidle0', timeout: 0 });
-
-            const element = await page.$('#root');
-
-            if (!element) {
-                throw new Error('No body element was found to screenshot.');
-            }
-
-            return await element.screenshot({
-                type: 'png',
-                optimizeForSpeed: true,
-                omitBackground: transparency,
-                path: savePath
-            });
-        });
+    constructor(browser: Browser) {
+        this._browser = browser;
     }
 
     /**
@@ -38,15 +19,9 @@ export default class HTMLImageProcessor {
      * @param options The custom options you want for puppeteer.
      * @returns {Promise<HTMLImageProcessor>}
      */
-    static async launch(options?: LaunchOptions): Promise<HTMLImageProcessor> {
-        const cluster = await Cluster.launch({
-            puppeteer: puppeteer,
-            concurrency: Cluster.CONCURRENCY_PAGE,
-            maxConcurrency: 5,
-            puppeteerOptions: options,
-        });
-
-        return new this(cluster);
+    static async launch(options: ConnectOptions = {}): Promise<HTMLImageProcessor> {
+        const browser = await puppeteer.connect(options);
+        return new this(browser);
     }
 
     /**
@@ -54,7 +29,7 @@ export default class HTMLImageProcessor {
      * @returns {Promise<void>}
      */
     public async close(): Promise<void> {
-        await this._cluster.close();
+        await this._browser.close();
     }
 
     /**
@@ -63,13 +38,22 @@ export default class HTMLImageProcessor {
      * @returns {Promise<Buffer>} The image buffer.
      */
     public async draw<T extends any>(options: DrawOptions<T>): Promise<Buffer> {
-        const template = Handlebars.compile<T>(options.html);
-        const result = template(options.handlebars || {} as T);
+        const page = await this._browser.newPage();
+        await page.setContent(options.html);
 
-        const { html, handlebars, ...payload } = options;
-        const image: Uint8Array = await this._cluster.execute({ html: result, ...payload });
+        const element = await page.$('#root');
 
-        await this._cluster.idle();
-        return Buffer.from(image);
+        if (!element) {
+            throw new Error('No body element was found to screenshot.');
+        }
+
+        const image = Buffer.from(await element.screenshot({
+            type: 'png',
+            omitBackground: true
+        }))
+
+        await page.close();
+        
+        return image;
     }
 }
