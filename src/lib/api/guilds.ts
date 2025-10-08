0c0133ecc7637593ca7f193364f57eea47b4d576
LuckFire <39076891+LuckFire@users.noreply.github.com>
2025-10-08 03:04:28 -0400

    refactor: guild requests

--- src/lib/api/guilds.ts
+++ src/lib/api/guilds.ts
@@ -1,11 +1,12 @@
 import { okAsync, errAsync } from 'neverthrow';
-import type { APIResponse, GuildSettings, GuildActivityTrackingUpdate, GuildActivityRoleCreate } from '#/lib/types/api';
+import type { APIResponse, APIError, GuildSettings, GuildActivityTrackingUpdate, GuildActivityRoleCreate } from '#/lib/types/api';
 import { request } from '#/lib/util/request';
+import RequestError from '#/lib/extensions/RequestError';
 
 const { BOT_API_URL, BOT_ENDPOINT_API_KEY } = process.env
 
 export async function createGuildSettings(guildId: string) {
-    const res = await request<APIResponse<GuildSettings>>({
+    const res = await request<APIResponse<GuildSettings>, APIError>({
         url: new URL(`/v1/guild/${guildId}/settings`, BOT_API_URL),
         method: 'POST',
         headers: {
@@ -19,7 +20,7 @@ export async function createGuildSettings(guildId: string) {
 }
 
 export async function getGuildSettings(guildId: string, { create }: { create?: boolean } = {}) {
-    const res = await request<APIResponse<GuildSettings>>({
+    const res = await request<APIResponse<GuildSettings>, APIError>({
         url: new URL(`/v1/guild/${guildId}/settings`, BOT_API_URL),
         method: 'GET',
         headers: {
@@ -28,16 +29,24 @@ export async function getGuildSettings(guildId: string, { create }: { create?: b
     });
 
     if (res.isErr()) {
-        if (!create) return res;
+        if (!(res.error instanceof RequestError)) {
+            return res;
+        }
 
-        return createGuildSettings(guildId);
+        if (res.error.response.status === 404) {
+            if (!create) return res;
+
+            return createGuildSettings(guildId);
+        }
+
+        return res;
     }
 
     return okAsync(res.value.data);
 }
 
 export async function updateGuildActivitySettings(guildId: string, settings: GuildActivityTrackingUpdate) {
-    const res = await request<APIResponse<GuildSettings>>({
+    const res = await request<APIResponse<GuildSettings>, APIError>({
         url: new URL(`/v1/guild/${guildId}/settings/activity`, BOT_API_URL),
         method: 'PATCH',
         headers: {
@@ -50,7 +59,7 @@ export async function updateGuildActivitySettings(guildId: string, settings: Gui
 }
 
 export async function createAcitivtyRole(guildId: string, options: GuildActivityRoleCreate) {
-    const res = await request<APIResponse<GuildSettings>>({
+    const res = await request<APIResponse<GuildSettings>, APIError>({
         url: new URL(`/v1/guild/${guildId}/settings/activity-roles`, BOT_API_URL),
         method: 'POST',
         headers: {
